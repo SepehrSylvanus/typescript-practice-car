ca0018f11d5f60c108665df2ede8c8bda6237042
Sylvanus-the-sorcerer <103826382+SylvanusSlytherin@users.noreply.github.com>
2024-01-19 11:59:34 +0330

    layout

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,21 +1,22 @@
-'use client'
-import { CustomButtonProps } from '@/types'
-import Image from 'next/image'
-const CustomButton = ({title, containerStyles, handleClick}: CustomButtonProps) => {
+"use client";
+import { CustomButtonProps } from "@/types";
+import Image from "next/image";
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+  btnType,
+}: CustomButtonProps) => {
   return (
-   <button
-   disabled={false}
-   type={'button'}
-   className={`custom-btn ${containerStyles}`}
-   onClick={handleClick}
-   >
-<span className={`flex-1`}>
-  {title}
-</span>
+    <button
+      disabled={false}
+      type={btnType || "button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1`}>{title}</span>
+    </button>
+  );
+};
 
-
-   </button>
-  )
-}
-
-export default CustomButton
+export default CustomButton;
