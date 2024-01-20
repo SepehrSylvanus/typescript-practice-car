1523c3653188a81b5502b24c316c746ba9f939c1
Sylvanus-the-sorcerer <103826382+SylvanusSlytherin@users.noreply.github.com>
2024-01-20 17:41:51 +0330

    server side rendering

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,13 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+  const {manufacturer, year, model, limit, fuel} = filters
   const headers = {
     "X-RapidAPI-Key": "c7fbb0f41emsh7b344c2b3e69f27p13df66jsndf287bd29bb6",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
   const response = await fetch(
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3",
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limi=${limit}&fuel_type=${fuel }`,
     {
       headers: headers,
     }
